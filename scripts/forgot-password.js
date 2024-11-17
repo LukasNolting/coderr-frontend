40704ec32d6fc571101ffc2549f1bdd3b60f5700
Lukas Nolting <info@lukas-nolting.eu>
2024-11-17 15:59:43 +0100

    registration process & reset-password mail

--- scripts/forgot-password.js
+++ scripts/forgot-password.js
@@ -11,3 +11,36 @@ function forgotPasswordSubmit(event) {
 
   forgotPasswordSubmit(data);
 }
+
+
+async function onSubmit(event) {
+  event.preventDefault();
+  let email = document.getElementById('email').value;
+  await forgotPassword(email);
+      
+}
+
+async function forgotPassword(email) {
+  const body = {email};
+  const link = `${PASSWORD_RESET_URL}`;
+  try {
+    const response = await postDataWJSON(link, body);
+    if (response.ok) {
+    forgotPasswordSuccess();
+    }
+    else {
+      showToastMessage(true, ['E-Mail konnte nicht gesendet werden.'])
+    }
+  } catch(error) {
+    console.error('Request-Fehler:', error);
+  }
+}
+
+function forgotPasswordSuccess() {
+  document.getElementById('forgot-password-form').innerHTML = `
+          <div class="forgot-password-success-container">
+            <h2>E-Mail erfolgreich gesendet!</h2>
+            <p>Bitte prüfe deine E-Mails.</p>
+            <img src="/assets/img/icons/email.svg" alt="success">
+        </div>`
+}
