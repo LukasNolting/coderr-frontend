c122f6e109ebe167a245aca9371b81a9d15b5af5
Lukas Nolting <info@lukas-nolting.eu>
2024-11-21 21:53:43 +0100

    offers display

--- shared/scripts/template/offer_templates.js
+++ shared/scripts/template/offer_templates.js
@@ -66,7 +66,7 @@ function getOfferTemplate(offer){
                 <div onclick="redirectToOffer(${offer.id})" class="offer_card d_flex_cs_gm f_d_c">
                     <img class="offer_image" src="${getOfferImgPath(offer.image)}" alt="Angebotsbild">
                     <div class="d_flex_cs_gm f_d_c">
-                        <h3>${offer.title}</h3>
+                        <h3 class="text_ellipsis">${offer.title}</h3>
                         <p>${offer.description}</p>
                         <p>${getUpdateOrCreateDate(offer)}</p>
                         <div class="d_flex_cs_gm f_d_c">
