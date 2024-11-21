f4462ab25c76ac336dd0f02f247c6cb177d5152e
Lukas Nolting <info@lukas-nolting.eu>
2024-11-21 18:08:56 +0100

    activate account toast

--- scripts/login.js
+++ scripts/login.js
@@ -1,17 +1,28 @@
-function checkAuthLogin(){
-    if(getAuthToken()){
+function checkAuthLogin() {
+    if (getAuthToken()) {
         window.location.href = "./index.html";
-    } 
+    }
     setHeader()
+    activateSuccess()
 }
 
-function logInSubmit(event){
+function logInSubmit(event) {
     event.preventDefault();
     const data = getFormData(event.target);
 
     logIn(data);
 }
 
-function guestLogin(type){
+function guestLogin(type) {
     logIn(GUEST_LOGINS[type])
+}
+
+function activateSuccess() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const msg = urlParams.get('msg');
+
+    if (msg) {
+        showToastMessage(false, ['Ihr Account wurde aktiviert!']);
+    }
 }
